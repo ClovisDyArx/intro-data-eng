32f7d66224b28cc155b96a740c7888112fcbbad1
Clovis Lechien <clovis.lechien@epita.fr>
2024-06-11 15:46:06 +0200

    feat(step1): LETS GOOOO IT WORKS, IT REALLY DO YOUUUUUUUUUUUUHOUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- gen_data/build.sbt
+++ gen_data/build.sbt
@@ -2,12 +2,21 @@ name := "DroneDataProducer"
 
 version := "0.1"
 
-scalaVersion := "2.13.8"
+scalaVersion := "2.13.6"
 
 libraryDependencies ++= Seq(
   "org.apache.kafka" %% "kafka" % "2.8.0",
-  "org.apache.kafka" %% "kafka-clients" % "2.8.0"
+  "org.apache.kafka" % "kafka-clients" % "2.8.0",
+  "org.apache.kafka" %% "kafka-streams-scala" % "2.8.0",
+  "org.scalatest" %% "scalatest" % "3.2.10" % Test,
 )
 
-mainClass in (Compile, run) := Some("DroneDataProducer")
+
+
+resolvers ++= Seq(
+  "Confluent" at "https://packages.confluent.io/maven/",
+  "Apache Repo" at "https://repo1.maven.org/maven2/"
+)
+
+mainClass in Compile := Some("DroneDataProducer")
 
--- gen_data/src/main/scala/DroneDataProducer.scala
+++ gen_data/src/main/scala/DroneDataProducer.scala
@@ -2,38 +2,39 @@ import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
 object DroneDataProducer extends App {
-  val (config, topic) = ProducerConfig.getConfig("kafka-intro.conf")
+  val bootstrapServers = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
+  val topic = "quickstart-events"
+  val interval = 1000
 
-  val producer = new KafkaProducer[String, String](config)
+  val props = new Properties()
+  props.put("bootstrap.servers", bootstrapServers)
+  props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+  props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
 
-  val interval = 1000 // todo: adapter l'intervalle de temps entre les générations de données.
+  val producer = new KafkaProducer[String, String](props)
 
   while (true) {
     val droneInfo = DroneDataGenerator.generateDroneInfo()
     val record = new ProducerRecord[String, String](topic, droneInfo.id, droneInfo.toJsonString)
-
     producer.send(record)
     println(s"Sent: $droneInfo")
-
-    Thread.sleep(interval)
+    Thread.sleep(interval) // toutes les 'interval' ms.
   }
+  // producer.close() // generation infinie
 
-  producer.close()
-}
-
-implicit class DroneInfoJson(droneInfo: DroneInfo) {
-  def toJsonString: String = {
-    s"""
-       |{
-       | "id": "${droneInfo.id}",
-       | "created": "${droneInfo.created}",
-       | "latitude": ${droneInfo.latitude},
-       | "longitude": ${droneInfo.longitude},
-       | "event_type": "${droneInfo.event_type}",
-       | "danger_level": ${droneInfo.danger_level},
-       | "survivors": ${droneInfo.survivors}
-       |}
-     """.stripMargin
+  implicit class DroneInfoJson(droneInfo: DroneInfo) {
+    def toJsonString: String = {
+      s"""
+         |{
+         | "id": "${droneInfo.id}",
+         | "created": "${droneInfo.created}",
+         | "latitude": ${droneInfo.latitude},
+         | "longitude": ${droneInfo.longitude},
+         | "event_type": "${droneInfo.event_type}",
+         | "danger_level": ${droneInfo.danger_level},
+         | "survivors": ${droneInfo.survivors}
+         |}
+       """.stripMargin
+    }
   }
 }
-
