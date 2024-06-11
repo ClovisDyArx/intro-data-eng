bf34aeb81eeb4e1fafd42af742fb713c05b20e0d
Clovis Lechien <clovis.lechien@epita.fr>
2024-06-11 14:50:35 +0200

    feat(step1): added the drone data generator that instances new drone data randomly

---
+++ gen_data/src/main/scala/DroneDataGenerator.scala
@@ -0,0 +1,19 @@
+import java.time.LocalDate
+import scala.util.Random
+
+object DroneDataGenerator {
+  private val eventTypes = Array("earthquake", "tsunami", "wildfire", "flood", "attack", "landslide", "tornado", "drought", "cyclone", "avalanche")
+
+  def generateDroneInfo(): DroneInfo = {
+    DroneInfo(
+      id = Random.alphanumeric.take(10).mkString, //ex: S69E69X69Y
+      created = LocalDate.now(), // timestamp
+      latitude = Random.between(-90.0f, 90.0f), // [-90.0, 90.0]
+      longitude = Random.between(-180.0f, 180.0f), // [-180.0, 180.0]
+      event_type = eventTypes(Random.nextInt(eventTypes.length)), // ex: tsunami
+      danger_level = Random.nextInt(6), // [0, 5]
+      survivors = Random.nextInt(21) // [0, 20]
+    )
+  }
+}
+
