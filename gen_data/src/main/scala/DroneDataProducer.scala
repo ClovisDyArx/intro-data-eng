27cb4368546b9a883af02a055dfa3a6322551520
Clovis Lechien <clovis.lechien@epita.fr>
2024-06-18 19:01:31 +0200

    feat(drone): removed the FORBIDDEN while, and added futures to send data periodically, doesnt fully work like I would want to but it still sends data so its fine for now.

--- gen_data/src/main/scala/DroneDataProducer.scala
+++ gen_data/src/main/scala/DroneDataProducer.scala
@@ -1,10 +1,15 @@
 import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._ // NE PAS TOUCHER : si on retire ça, ça envoie rien ??????
+
 
 object DroneDataProducer extends App {
   val bootstrapServers = sys.env.getOrElse("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092")
   val topic = "quickstart-events"
-  val interval = 1000
+  val interval = 1000 // useless maintenant
+  val batchSize = 500 // envoie 500 messages.
 
   val props = new Properties()
   props.put("bootstrap.servers", bootstrapServers)
@@ -13,14 +18,37 @@ object DroneDataProducer extends App {
 
   val producer = new KafkaProducer[String, String](props)
 
-  while (true) {
-    val droneInfo = DroneDataGenerator.generateDroneInfo()
-    val record = new ProducerRecord[String, String](topic, droneInfo.id, droneInfo.toJsonString)
-    producer.send(record)
-    println(s"Sent: $droneInfo")
-    Thread.sleep(interval) // toutes les 'interval' ms.
+  def generateAndSendBatch(): Future[Unit] = {
+    val batch = List.fill(batchSize)(DroneDataGenerator.generateDroneInfo())
+
+    val records = batch.map { droneInfo =>
+      new ProducerRecord[String, String](topic, droneInfo.id, droneInfo.toJsonString)
+    }
+
+    val futures = records.map { record =>
+      Future {
+        producer.send(record)
+        println(s"Sent: ${record.value()}") // debug: afficher les messages envoyés
+      }
+    }
+
+    Future.sequence(futures).map(_ => ())
   }
-  producer.close() // generation infinie
+
+  def scheduleSendBatches(): Future[Unit] = {
+    Future {
+      generateAndSendBatch()
+      // Thread.sleep(interval) // aucun effet sur le nouveau code, donc useless
+    }.flatMap { _ =>
+      scheduleSendBatches()
+    }
+  }
+
+  scheduleSendBatches()
+
+  sys.addShutdownHook {
+    producer.close()
+  }
 
   implicit class DroneInfoJson(droneInfo: DroneInfo) {
     def toJsonString: String = {
